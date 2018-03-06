f04828f46ee68a9eb01ee23dbfd2649430c9524a
kazambient <kazambient@gmail.com>
2018-03-05 19:34:23 -0800

    first

---
+++ app.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(80);
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+  var timer = setInterval(function(){
+      socket.emit('news',{hello:Date.now()});
+  },1000);
+  
+  
+
+  
+
+});
